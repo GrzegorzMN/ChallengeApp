ceb2419bc6815181fcd506662670b27026fa309d
Grzegorz Multan <grzegorz.multan@gmail.com>
2023-12-02 20:22:13 +0100

    Zadanie Dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///////////////////
+//Program Dzien 4//
+///////////////////
+
+//Zmienne
+var Name = "Ewa";
+var Sex = 'K';
+var Age = 25;
+
+if (Sex == 'K')
+{
+    if (Name == "Ewa" && Age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (Age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Inna piękna kobieta");
+    }
+}
+else
+{
+    if(Age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else if (Age > 18 && Name == "Grzegorz")
+    {
+        Console.WriteLine("Pełnoletni Grzegorz");
+    }
+    else
+    {
+        Console.WriteLine("Inny mężczyzna");
+    }
+}
